4ec34346533382e389e2549b3aaf2d35d0f9529c
Alejandro <alexalejandroem@gmail.com>
2019-09-04 15:39:19 +0200

    Retrive todos from database, added checkbox for checked task and added remove method

--- client/main.js
+++ client/main.js
@@ -1,7 +1,6 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import todosList from '../imports/components/todosList/todosList';
-import '../imports/api/tasks.js';
 
 angular.module('simple-todos', [
   angularMeteor,
--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -10,9 +10,37 @@ class TodosListCtrl {
 
         this.helpers({
             tasks() {
-                return Tasks.find({});
+                return Tasks.find({}, {
+                    sort: {
+                        createdAt: -1
+                    }
+                });
             }
         })
+    }
+
+    addTask(newTask) {
+        // Insert a task into the collection
+        Tasks.insert({
+            text: newTask,
+            createdAt: new Date
+        });
+
+        // Clear form
+        this.newTask = '';
+    }
+
+    setChecked(task) {
+        // Set the checked property to the opposite of its current value
+        Tasks.update(task._id, {
+            $set: {
+                checked: !task.checked
+            },
+        });
+    }
+
+    removeTask(task) {
+        Tasks.remove(task._id);
     }
 }
 
